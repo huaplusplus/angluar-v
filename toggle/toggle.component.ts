fb7f7c8fe497a1d15e8e5fd8a6b9783ac0c9d5ff
huaplusplus <huaplusplus@163.com>
2018-08-01 23:20:28 +0800

    use bi-direction data binding, todo: need a module to integrate all compoents

--- toggle/toggle.component.ts
+++ toggle/toggle.component.ts
@@ -1,33 +1,42 @@
-import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
+import {Component, OnInit, Input, Output, Injector, forwardRef} from '@angular/core';
+import {ControlValueAccessor, NG_VALUE_ACCESSOR} from '@angular/forms';
 
 @Component({
   selector: 'v-toggle',
   styleUrls: ['./toggle.component.css'],
-  templateUrl: './toggle.component.html'
+  templateUrl: './toggle.component.html',
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => ToggleComponent),
+    multi: true,
+  }]
+
 })
 
-export class ToggleComponent implements OnInit {
-  @Input() value: boolean;
+export class ToggleComponent implements ControlValueAccessor {
   @Input() disabled: boolean = false;
   @Input() fontSize: number = 25;
   @Output()
-  change: EventEmitter<any> = new EventEmitter();
-  curValue:boolean;
-
-
-
-  constructor() {
-  }
-
-  ngOnInit() {
-    this.curValue = !!this.value;
-  }
+  curValue: boolean;
+  toggleChangeFn: Function;
 
   onToggle() {
     this.curValue = !this.curValue;
-    this.change.emit(this.curValue);
+    this.toggleChangeFn(this.curValue);
   }
 
+  writeValue(value) {
+    this.curValue = value;
+  }
+
+
+  registerOnChange(fn: any) {
+    this.toggleChangeFn = fn;
+  }
+
+  registerOnTouched(fn: any) {
+
+  }
 
 }
 
